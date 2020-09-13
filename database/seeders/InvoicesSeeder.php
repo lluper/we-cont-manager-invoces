3a2ebe5425a086fc2a398d96b506c911aa4fbfcb
lluper <leonardoluper5@gmail.com>
2020-09-13 10:42:52 -0300

    create InvoiceSeeder

--- database/seeders/InvoicesSeeder.php
+++ database/seeders/InvoicesSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
-class InvocesSeeder extends Seeder
+class InvoicesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
